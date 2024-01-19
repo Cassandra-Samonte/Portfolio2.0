010caf6a019e0fba97cd1d8237cf3128d02169ea
Cas <cssamonte@gmail.com>
2024-01-19 14:38:37 -0800

    added projects and responsive grid

--- Portfolio/src/componenets/Home/index.jsx
+++ Portfolio/src/componenets/Home/index.jsx
@@ -1,10 +1,15 @@
 export default function Home() {
     return (
         <>
-            <h1 className="hello">Hello</h1>
-            <h2 className="name">I'm Cassandra</h2>
-            <h3 className="title">A Software Engineer</h3>
-            <h4 className="tagline pb-36">with a passion for fontend design</h4>
+            <div className="h-screen relative">
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <h1 className="hello">Hello</h1>
+                    <h2 className="name">I'm Cassandra</h2>
+                    <h3 className="title">A Software Engineer</h3>
+                    <h4 className="tagline">with a passion for frontend design</h4>
+                </div>
+            </div>
         </>
     )
 }
+
--- Portfolio/src/componenets/Projects/index.jsx
+++ Portfolio/src/componenets/Projects/index.jsx
@@ -1,7 +1,16 @@
-export default function ProjectsGallery () {
+
+export default function Projects() {
     return (
         <>
-            <h1 className="titlesection">Projects</h1>
+            <div className="h-screen relative">
+                <h1 className="titlesection">Projects</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 px-4 md:px-10 lg:px-20 gap-4">
+                    <img src="/images/world.png" alt="Project 1" />
+                    <img src="/images/parks.png" alt="Project 2" />
+                    <img src="/images/vapor.png" alt="Project 3" />
+                    <img src="/images/tempo.png" alt="Project 4" />
+                </div>
+            </div>
         </>
     )
 }
