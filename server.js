5f47083c71230507a584d2bf540cb2292d281fa5
Cas <cssamonte@gmail.com>
2024-01-20 15:04:57 -0800

    server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -12,3 +12,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
