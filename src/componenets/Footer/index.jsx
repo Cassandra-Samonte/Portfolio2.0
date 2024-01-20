205c432f1cd0e960b47b7c52cb00be2d74321c93
Cas <cssamonte@gmail.com>
2024-01-20 15:15:29 -0800

    updated resume link

--- src/componenets/Footer/index.jsx
+++ src/componenets/Footer/index.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
           <a href="https://www.linkedin.com/in/cassandra-samonte/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">LinkedIn</a>
         </li>
         <li>
-          <a href="https://drive.google.com/file/d/1oPc9IlPGzCqwij1D57rf2pHZ7q8kH6Q8/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">Resume</a>
+          <a href="https://drive.google.com/file/d/1-HCXr551YEBE0iKzCnaqQN10YrnPqF_G/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">Resume</a>
         </li>
       </ul>
     </footer>
